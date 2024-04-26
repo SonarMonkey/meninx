7444ddfb5ed82720cd2b59a6b62c21c69db96f18
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-04-26 16:33:29 -0600

    Oops

--- home/shared/vscode.nix
+++ home/shared/vscode.nix
@@ -21,7 +21,7 @@
       yzhang.markdown-all-in-one
       ms-vscode.cpptools-extension-pack
       ms-vscode-remote.remote-ssh
-      vscode-extensions.gruntfuggly.todo-tree
+      gruntfuggly.todo-tree
     ];
 
     # General settings mostly taken from piousdeer
