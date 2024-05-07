506c704fe727007d0cbe94b1229c34a811ea30bb
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-05-07 16:25:55 -0600

    Remove warp terminal

--- nix/encephalon/experiments.nix
+++ nix/encephalon/experiments.nix
@@ -5,10 +5,4 @@
 }: {
   # Install ollama for running local LLMs
   services.ollama.enable = true;
-
-  # Packages to play around with
-  environment.systemPackages = with pkgs; [
-    # My personally-updated warp terminal because it's fun
-    warp-terminal
-  ];
 }
