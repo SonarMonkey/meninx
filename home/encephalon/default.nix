62a5a7b5ed878b4958ecf800d082515053c26bc4
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-05-07 14:45:37 -0600

    Disable autostart (temporarily)

--- home/encephalon/default.nix
+++ home/encephalon/default.nix
@@ -8,9 +8,11 @@
     ../shared
 
     # Encephalon-specific configuration
-    ./autostart.nix
     ./dconf.nix
     ./gnome.nix
+
+    # FIXME disable temporarily
+    #./autostart.nix
 
     # Catpuccin theme config
     # FIXME disable for GNOME 46
