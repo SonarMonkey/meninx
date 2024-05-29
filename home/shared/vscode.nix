047f7954df1de24ee2c0532e6fb1bfc78191cf75
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-05-29 14:31:06 -0600

    Fix wrong attrset for codeium extension

--- home/shared/vscode.nix
+++ home/shared/vscode.nix
@@ -26,7 +26,7 @@
       pkgs.vscode-extensions.yzhang.markdown-all-in-one
 
       # Extensions from nix-vscode-extenions
-      pkgs.vscode-extensions.codeium.codeium
+      pkgs.vscode-marketplace.codeium.codeium
     ];
 
     # General settings mostly taken from piousdeer
