fe2410c237cf03e98f5345e9c67d270f4132f540
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-06-04 15:16:47 -0600

    Remove ollama ui

--- nix/encephalon/experiments.nix
+++ nix/encephalon/experiments.nix
@@ -5,7 +5,6 @@
 }: {
   # Install ollama for running local LLMs
   services.ollama.enable = true;
-  services.nextjs-ollama-llm-ui.enable = true;
 
   # Stuff from charm.sh for scripts and experiments
   environment.systemPackages = with pkgs; [
