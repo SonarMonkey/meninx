33aba62ca01d20cf0e8d589ba8118daba90fee3d
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-06-04 15:55:15 -0600

    Add dwarf fortress

--- nix/encephalon/experiments.nix
+++ nix/encephalon/experiments.nix
@@ -8,9 +8,13 @@
 
   # Stuff from charm.sh for scripts and experiments
   environment.systemPackages = with pkgs; [
+    # Utils and programs to play with
     warp-terminal
     charm-freeze
     glow
     gum
+
+    # Dwarf fortress "full" pack
+    dwarf-fortress-packages.dwarf-fortress-full
   ];
 }
