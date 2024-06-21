80230d0f677e65e7084d337039535b2446fc4fd3
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-06-21 12:51:35 -0600

    Remove unused packages

--- nix/encephalon/experiments.nix
+++ nix/encephalon/experiments.nix
@@ -9,11 +9,6 @@
   # Stuff from charm.sh for scripts and experiments
   environment.systemPackages = with pkgs; [
     # Charm utils
-    charm-freeze
-    glow
     gum
-
-    # Dwarf fortress "full" pack
-    dwarf-fortress-packages.dwarf-fortress-full
   ];
 }
