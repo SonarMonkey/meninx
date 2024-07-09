eeb8dfedcf4e533b9cd1214596667b286f967f75
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-07-09 10:57:13 -0600

    Disable hplip until fix is built

--- nix/encephalon/default.nix
+++ nix/encephalon/default.nix
@@ -10,7 +10,7 @@
 
     # Pre-defined hardware configs
     inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
-    inputs.nixos-hardware.nixosModules.common-cpu-intel-kaby-lake
+    inputs.nixos-hardware.nixosModules.common-gpu-intel-kaby-lake
 
     # Encephalon-specific configuration
     ./boot.nix
