38f3a3bc06f71ed3b96a603720a52b2457cfe4d0
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-08-01 13:22:08 -0600

    Disable unused local packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,12 +2,12 @@
 # You can build them using 'nix build .#example'
 {pkgs}: {
   # example = pkgs.callPackage ./example { };
-  warp-terminal = pkgs.callPackage ./warp-terminal {};
-  dnd-tools = pkgs.callPackage ./dnd-tools {};
+  #warp-terminal = pkgs.callPackage ./warp-terminal {};
+  #dnd-tools = pkgs.callPackage ./dnd-tools {};
   #mullvad-vpn = pkgs.callPackage ./mullvad-vpn {};
   zed-editor = pkgs.callPackage ./zed-editor {};
   heynote = pkgs.callPackage ./heynote {};
   #beaver-notes = pkgs.callPackage ./beaver-notes {};
   #anytype = pkgs.callPackage ./anytype {};
-  affine = pkgs.callPackage ./affine {};
+  #affine = pkgs.callPackage ./affine {};
 }
