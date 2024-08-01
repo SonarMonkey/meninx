90d0229efbfd9c64a697c6945261bfdf26ce9fe4
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-08-01 13:25:27 -0600

    Heynote v1.7.1 -> v1.8.0

--- pkgs/heynote/default.nix
+++ pkgs/heynote/default.nix
@@ -8,7 +8,7 @@
 
   src = fetchurl {
     url = "https://github.com/heyman/heynote/releases/download/v${version}/Heynote_${version}_x86_64.AppImage";
-    sha256 = "sha256-/ppKcx8ZY6Y403cBNEAXFw1mBHPPCTSlFOWf+UeMvnY=";
+    sha256 = "sha256-NzrXV8HmCPYE+D3tEwVv3rBkLF0/FKW6uJdqhKmH8uw=";
   };
 
   appimageContents = appimageTools.extract {
