e214106d5c7707d1e62054510f41222ebef055d7
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-08-07 16:31:25 -0600

    Rename files to prep for switching zsh configs

--- home/shared/zsh.nix
+++ home/shared/zsh.nix
@@ -3,13 +3,34 @@
   pkgs,
   ...
 }: {
-  # Configure zsh + prezto
+  # Install and configure zsh
   programs.zsh = {
-    # Basic settings
+    # Enable and basic settings
     enable = true;
+    enableCompletion = true;
+    enableVteIntegration = true;
     autocd = true;
 
-    # Completions and fancy pipe
+    # Enable and configure autosuggest
+    autosuggestion = {
+      enable = true;
+      strategy = [
+        "history"
+        "completion"
+      ];
+    };
+
+    # Increase histsize and configure
+    history = {
+      extended = true;
+      save = "100000";
+      size = "100000";
+    };
+
+    # Enable default substring search
+    historySubstringSearch.enable = true;
+
+    # Initialize batpipe and nh completions
     initExtra = ''
       eval "$(batpipe)"
       eval "$(nh completions -s zsh)"
@@ -20,82 +41,13 @@
       NIXOS_OZONE_WL = "1";
     };
 
-    # Shell aliases
-    shellAliases = {
-      # General stuff
-      e = "$EDITOR";
-      se = "sudo $EDITOR";
-      h = "hx";
-      ns = "nix-env -qa | fzf";
-      s = "kitten ssh";
-
-      # Tailscale
-      ts = "tailscale";
-      tss = "tailscale status";
-      tnet = "tailscale netcheck";
-      trec = "tailscale down && tailscale up --ssh --operator=sonar";
-      tre = "sudo systemctl restart tailscaled.service systemd-resolved.service";
-
-      # Tailscale VPN-specific
-      vpndown = "tailscale set --exit-node ''";
-      vpnden = "tailscale set --exit-node '100.120.178.99'";
-      vpnse = "tailscale set --exit-node '100.127.8.7'";
-
-      # Mullvad
-      mu = "mullvad";
-      mus = "mullvad status";
-      mup = "mullvad connect";
-      mur = "mullvad reconnect";
-      mud = "mullvad disconnect";
-      mue = "mullvad-exclude";
-
-      # Nix configs
-      ne = "e ~/Nix/";
-      nc = "e ~/Nix/configuration.nix";
-      ndot = "e ~/Nix/nix.d/";
-      he = "e ~/Nix/home.nix";
-      hdot = "e ~/Nix/home.d/";
-
-      # nixOS
-      nfm = "nix fmt && git commit -a -m \"Format with Alejandra\"";
-      fup = "nix flake --refresh update --commit-lock-file";
-      fch = "nix flake check";
-      nup = "sudo nixos-rebuild switch -v";
-      nbo = "sudo nixos-rebuild boot -v";
-      gcol = "sudo nix-collect-garbage -d";
-      nopt = "sudo nix-store -j 8 -vv --optimise";
-
-      # Nix-helper
-      switch = "nh os switch --ask";
-      boot = "nh os boot --ask";
-      search = "nh search";
-    };
-
-    # Prezto config
-    prezto = {
+    # Enable and configure default syntax highlighter
+    syntaxHighlighting = {
       enable = true;
-
-      # Editor configuration
-      editor = {
-        dotExpansion = true;
-        keymap = "vi";
-        promptContext = true;
-      };
-
-      # Modules to load
-      pmodules = [
-        "environment" # must be loaded first
-        "archive" # must come before "completion"
-        "editor" # not sure what this is
-        "git" # must come before "completion"
-        "history" # maybe not necessary? needs addl. config
-        "syntax-highlighting" # not sure if redundant
-        "history-substring-search" # must be loaded after "syntax-highlighting"
-        "autosuggestions" # must before loaded after "syntax-highlighting" and "history-substring-search"
-        "prompt" # just for themes? needs addl. config
-        "spectrum" # improves 256-color support
-        "utility" # must be loaded before "completion"
-        "completion" # must be loaded after "utility"
+      highlighters = [
+        "main"
+        "brackets"
+        "cursor"
       ];
     };
   };
