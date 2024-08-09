9c11e5e4460fb4ddecf16e0dc59864816106145d
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-08-09 13:53:39 -0600

    configure rio

--- home/encephalon/rio.nix
+++ home/encephalon/rio.nix
@@ -7,5 +7,13 @@
   programs.rio = {
     enable = true;
     package = inputs.rio.packages.x86_64-linux.default;
+    settings = {
+      editor = "hx";
+      cursor = "█";
+      blinking-cursor = true;
+      fonts = {
+        family = "Maple Mono NF";
+      };
+    };
   };
 }
