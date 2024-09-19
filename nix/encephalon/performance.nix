f0c279629fa88f20682fc45b977fe1db47411c01
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-09-19 11:03:37 -0600

    Consolidate gamemode config to performance.nix

--- nix/encephalon/performance.nix
+++ nix/encephalon/performance.nix
@@ -21,4 +21,25 @@
       interval = "monthly";
     };
   };
+
+  # Feral gamemode
+  programs.gamemode = {
+    # Basic settings
+    enable = true;
+
+    # May conflict with ananicy
+    enableRenice = false;
+
+    # Config
+    settings = {
+      general = {
+        reaper_freq = 5;
+        desiredgov = "performance";
+        softrealtime = "auto";
+        ioprio = 0;
+        inhibit_screensaver = 1;
+        disable_splitlock = 1;
+      };
+    };
+  };
 }
