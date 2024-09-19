a6c973ea3a8ad1ce41c59d654492dd437cc6393a
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-09-19 11:13:06 -0600

    Minor comment tidying

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,6 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
 {pkgs}: {
-  # example = pkgs.callPackage ./example { };
   #mullvad-vpn = pkgs.callPackage ./mullvad-vpn {};
   #mullvad = pkgs.callPackage ./mullvad {};
   #zed-editor = pkgs.callPackage ./zed-editor {};
