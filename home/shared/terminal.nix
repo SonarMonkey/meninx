947f563f24dfc985b0ad7f6f8c1397f56cd5aacc
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-09-19 10:50:17 -0600

    Consolidate bash config into terminal config

--- home/shared/terminal.nix
+++ home/shared/terminal.nix
@@ -50,4 +50,17 @@
       search = "nh search";
     };
   };
+
+  # Configure bash
+  programs.bash = {
+    # Basic settings
+    enable = true;
+    enableCompletion = true;
+    enableVteIntegration = true;
+
+    # Fancy stuff
+    initExtra = ''
+      eval "$(nh completions -s bash)"
+    '';
+  };
 }
