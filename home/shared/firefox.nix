98e362d7755fc7b293acde70166a2290a9651898
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-09-30 10:45:15 -0600

    Update hash

--- home/shared/firefox.nix
+++ home/shared/firefox.nix
@@ -168,7 +168,7 @@
 
   # Add the actual CSS tweaks
   home.file = let
-    currHash = "sha256-22HUmpBZm3E5dN+kgdj4S3qFtZrIvTbz5svkCt1+Ruo=";
+    currHash = "sha256-rnK3y7dyLo4hNPNqAnDEn+7C4Y54/8Gx6fxe/kp3yTA=";
   in {
     # Fix window controls
     ".mozilla/firefox/sonar/chrome/window_control_placeholder_support.css".text =
