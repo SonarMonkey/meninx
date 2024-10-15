4f9ceb14f52b91d6fc62ed37d63856fc0ab5a6ca
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-10-15 14:38:06 -0600

    Switch to packaging the .deb file and fix many errors

--- pkgs/twos/default.nix
+++ pkgs/twos/default.nix
@@ -1,38 +1,87 @@
 {
+  stdenv,
   lib,
-  appimageTools,
+  makeWrapper,
   fetchurl,
-}: let
-  pname = "twos";
+  glib,
+  nss,
+  nspr,
+  at-spi2-atk,
+  cups,
+  dbus,
+  libdrm,
+  gtk3,
+  pango,
+  cairo,
+  xorg,
+  mesa,
+  expat,
+  libxkbcommon,
+  alsa-lib,
+}:
+stdenv.mkDerivation rec {
+  name = "twos";
   version = "6.7.0";
 
   src = fetchurl {
-    url = "https://twos.s3.us-west-2.amazonaws.com/mac/Twos-${version}.AppImage";
-    sha256 = "sha256-g3tdGDFEVWAlMS4ZML7IXIUpJna7D03Ldoszi8/p9zw=";
+    url = "https://twos.s3.us-west-2.amazonaws.com/mac/Twos-${version}.deb";
+    sha256 = "";
   };
+  sourceRoot = ".";
+  unpackCmd = "dpkg-deb -x Twos-${version}.deb .";
 
-  appimageContents = appimageTools.extract {
-    inherit pname version src;
+  dontConfigure = true;
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp -R usr/share opt $out/
+
+    substituteInPlace $out/share/applications/twos.desktop --replace /opt/ $out/opt/
+    ln -s $out/opt/Twos/twos $out/bin/twos
+
+    runHook postInstall
+  '';
+
+  preFixup = let
+    libPath = lib.makeLibraryPath [
+      glib
+      nss
+      nspr
+      at-spi2-atk
+      cups.lib
+      dbus.lib
+      libdrm
+      gtk3
+      pango
+      cairo
+      xorg.libX11
+      xorg.libXcomposite
+      xorg.libXdamage
+      xorg.libXext
+      xorg.libXfixes
+      xorg.libXrandr
+      xorg.libxcb
+      mesa
+      expat
+      libxkbcommon
+      alsa-lib
+    ];
+  in ''
+    patchelf \
+      --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+      --set-rpath "${libPath}" \
+      $out/opt/Twos/twos
+  '';
+
+  meta = with lib; {
+    homepage = "https://www.twosapp.com/";
+    description = "Twos: Remember & Share *Things*";
+    license = licenses.unfree;
+    platforms = platforms.linux;
+    maintainers = [SonarMonkey];
+    mainProgram = "twos";
   };
-in
-  appimageTools.wrapType2 rec {
-    inherit pname version src;
-
-    extraInstallCommands = ''
-      install -m 444 -D ${appimageContents}/twos.desktop \
-        $out/share/applications/twos.desktop
-      install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/0x0/apps/twos.png \
-        $out/share/icons/hicolor/512x512/apps/twos.png
-      substituteInPlace $out/share/applications/twos.desktop \
-        --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${pname}'
-    '';
-
-    meta = with lib; {
-      description = "Twos: Remember & Share *Things*";
-      homepage = "https://github.com/heyman/twos/";
-      license = licenses.mit;
-      maintainers = with maintainers; [];
-      mainProgram = "twos";
-      platforms = platforms.all;
-    };
-  }
+}
