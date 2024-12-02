1427561d18ffbb4ea8618115bc9a474d214c6bde
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2024-12-02 12:20:52 -0700

    Update lix to latest version

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
 
     # Replacement nix implementation
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
