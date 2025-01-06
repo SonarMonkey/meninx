9f74b9ba1588f9143f3bc520f09712891d3a6d67
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2025-01-06 12:58:36 -0700

    Update heynote to 2.0.0

--- pkgs/heynote/default.nix
+++ pkgs/heynote/default.nix
@@ -4,11 +4,11 @@
   fetchurl,
 }: let
   pname = "heynote";
-  version = "1.8.0";
+  version = "2.0.0";
 
   src = fetchurl {
     url = "https://github.com/heyman/heynote/releases/download/v${version}/Heynote_${version}_x86_64.AppImage";
-    sha256 = "sha256-NzrXV8HmCPYE+D3tEwVv3rBkLF0/FKW6uJdqhKmH8uw=";
+    sha256 = "sha256-wMpOEEWJpkZtuyq8P8X+aFuUFx+HyKbhmPBaC80YxIo=";
   };
 
   appimageContents = appimageTools.extract {
