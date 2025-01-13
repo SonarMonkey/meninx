e1d77cf6dc3b15b11c312a83bbf70f516c810619
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2025-01-13 16:24:43 -0700

    Re-enable and fix yazi

--- home/shared/tools.nix
+++ home/shared/tools.nix
@@ -93,17 +93,9 @@
 
   # Yazi
   programs.yazi = {
-    enable = false;
+    enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
-    settings = {
-      manager = {
-        show_hidden = true;
-        sort_by = "modified";
-        sort_dir_first = true;
-        sort_reverse = true;
-      };
-    };
   };
 
   # Zoxide
