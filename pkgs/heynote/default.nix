c5294aa294169a6fe7cf116615f330d2cb31281f
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2025-05-02 19:42:23 -0600

    Update to 2.2.1

--- pkgs/heynote/default.nix
+++ pkgs/heynote/default.nix
@@ -4,11 +4,11 @@
   fetchurl,
 }: let
   pname = "heynote";
-  version = "2.1.1";
+  version = "2.2.1";
 
   src = fetchurl {
     url = "https://github.com/heyman/heynote/releases/download/v${version}/Heynote_${version}_x86_64.AppImage";
-    sha256 = "sha256-qiNQtCBERmGyJh9bRmOQEfkjYyZmPrAjAJl+839jO3M=";
+    sha256 = "sha256-IUy5EAtUk5+e64DC4VXXTgkyrG3mNJiItf1d4DR/fms=";
   };
 
   appimageContents = appimageTools.extract {
@@ -20,7 +20,7 @@ in
 
     extraInstallCommands = ''
       install -m 444 -D ${appimageContents}/heynote.desktop $out/share/applications/heynote.desktop
-      install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/0x0/apps/heynote.png \
+      install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/512x512/apps/heynote.png \
         $out/share/icons/hicolor/512x512/apps/heynote.png
       substituteInPlace $out/share/applications/heynote.desktop \
         --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${pname}'
