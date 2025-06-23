66ea48abd8f64360fe4aef57c7a010f984dc2cbf
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2025-06-23 13:48:01 -0600

    Change renamed options

--- nix/encephalon/gnome.nix
+++ nix/encephalon/gnome.nix
@@ -5,15 +5,12 @@
   ...
 }: {
   # Enable GDM+GNOME desktop
-  services.xserver = {
+  services = {
     # X11 server
-    enable = true;
+    xserver.enable = true;
 
     # GNOME display manager
-    displayManager.gdm = {
-      enable = true;
-      autoSuspend = false;
-    };
+    displayManager.gdm.enable = true;
 
     # GNOME desktop environment
     desktopManager.gnome.enable = true;
