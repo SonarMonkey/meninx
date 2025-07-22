170b98d3fd055c8494097f8b2f4e4fb45cf68cba
SonarMonkey <12001125+SonarMonkey@users.noreply.github.com>
2025-07-22 16:47:50 -0600

    Override base settings instead of having multiple specialisations

--- home/shared/themes.nix
+++ home/shared/themes.nix
@@ -1,28 +1,11 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: {
-  # Full adwaita theme
-  specialisation."adwaita".configuration = {
-    # Tell 'nh' about specialisation
-    xdg.dataFile."home-manager/specialisation".text = "adwaita";
-
-    # Fish shell theme
-    programs.fish.interactiveShellInit = ''
-      fish_config theme choose "Base16 Default Dark"
-    '';
-
-    # Terminal program themes
-    programs.helix.settings.theme = "adwaita-dark";
-    programs.bat.config.theme = "base16";
-    programs.ghostty.settings.theme = "Adwaita Dark";
-    programs.zellij.settings.theme = "ansi";
-    programs.kitty.themeFile = "adwaita_dark";
-  };
-
   # Solarized terminal applications
-  specialisation."solarized".configuration = {
+  specialisation."solarized".configuration = with lib.mkOverride; {
     # Tell 'nh' about specialisation
     xdg.dataFile."home-manager/specialisation".text = "solarized";
 
